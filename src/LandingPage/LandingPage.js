d98e01bc58f3dd4f1595d9d0581533e6eb337590
Timothy Kelly <kelltimo@oregonstate.edu>
2020-01-05 19:17:05 -0700

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/beer-glass.png'; 
+import './LandingPage.module.css'
 
 export function LandingPage() {
 	return (
 		<div>
-		<TopNav/>
+			<TopNav/>
+			<img src={logo} className='logo' alt='logo'/>
 		</div>
 		); 
 }
